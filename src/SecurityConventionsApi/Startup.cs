6066e2fdaafd59e194e759c47085ca228c371228
unknown <46085273+greyhamwoohoo@users.noreply.github.com>
2022-02-15 22:53:27 +1100

    Removing Swagger, changing launchSettings

--- src/SecurityConventionsApi/Startup.cs
+++ src/SecurityConventionsApi/Startup.cs
@@ -1,16 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SecurityConventionsApi
 {
@@ -28,10 +20,6 @@ namespace SecurityConventionsApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SecurityConventionsApi", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +28,6 @@ namespace SecurityConventionsApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SecurityConventionsApi v1"));
             }
 
             app.UseHttpsRedirection();
